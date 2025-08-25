4735546b18eecf62ab0e51d14493798a09029e98
DrPrime01 <drprime.dev@gmail.com>
2025-08-25 19:51:15 +0100

    initial push

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import ScrollTrigger  from 'gsap';
+import SplitText  from 'gsap';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+export default function App() {
+    return (
+        <main></main>
+    )
+}
