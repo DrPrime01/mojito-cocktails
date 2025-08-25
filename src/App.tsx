fc9861a1073c703e14427aa72c00e2777adbccfb
DrPrime01 <drprime.dev@gmail.com>
2025-08-25 20:54:58 +0100

    feat: implemented hero section and minor text/parallax animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import ScrollTrigger  from 'gsap';
-import SplitText  from 'gsap';
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar.tsx";
+import Hero from "./sections/hero.tsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function App() {
-    return (
-        <main></main>
-    )
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  );
 }
